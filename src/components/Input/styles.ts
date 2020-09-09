4264f49034bb95b2acbac163d6f419cb1dfe4e94
guvarallo <gustavo.varallo@live.com>
2020-09-09 12:56:45 -0300

    added tooltip

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip/index';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -18,17 +21,26 @@ export const Container = styled.div<ContainerProps>`
   align-items: center;
 
   ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
     props.isFocused &&
     css`
       color: #ff9000;
       border-color: #ff9000;
     `}
 
+  
+
   input {
     flex: 1;
     background: transparent;
     border: 0;
     color: #fff;
+
     &::placeholder {
       color: #86898b;
     }
@@ -43,5 +55,22 @@ export const Container = styled.div<ContainerProps>`
       css`
         color: #ff9000;
       `}
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
