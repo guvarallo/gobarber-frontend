bae4ee968727b3f250d45898139bf31b4b738958
guvarallo <gustavo.varallo@live.com>
2020-09-11 17:18:59 -0300

    added toasts styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import SignIn from './pages/SignIn/index';
 // import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer/index';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
